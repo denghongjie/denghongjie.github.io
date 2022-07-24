32751aa9d8ce062863676e951a75267e50642182
denghongjie <guyu903573189@qq.com>
2022-07-24 13:22:19 +0800

    data

--- website/cattool/static/js/data.js
+++ website/cattool/static/js/data.js
@@ -695,3 +695,19 @@ const viewlist=[{
         url:"https://www.pixelsquid.com/png",
     }]
 }]
+
+const devlist=[{
+    title:"SEO 工具",
+    children:[
+    {
+        title:"阿里云·可视化",
+        desc:"阿里云可视化看板",
+        imgurl:"./static/img/app/alyiyunkshihua.png",
+        url:"https://www.aliyun.com/activity/intelligent/datav-showcase?spm=a2c0j.140",
+    },{
+        title:"数字像素",
+        desc:"SSL证书",
+        imgurl:"./static/img/app/shuzixiangshu.png",
+        url:"https://alerts.httpscop.com/",
+    },]
+},]
