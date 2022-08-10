d58318e48bb22421eee2810cb1ed296c90654d59
denghongjie <guyu903573189@qq.com>
2022-08-10 10:54:04 +0800

    新资源

--- website/tszcloud/static/data/testData.js
+++ website/tszcloud/static/data/testData.js
@@ -5,40 +5,56 @@ const carddata=[{
   blocklist:[{
     labeltip:'全部模块',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/校审-智能版.png",
       name:"TSDP校审软件",
       vision:"v2.0",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/校审中心_A2--ICON.png",
       name:"校审管理中心",
       vision:"测试版",
+      type:4, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_02.png",
       name:"批注交流",
       vision:"易批注",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_17.png",
       name:"权限管理",
       vision:"企业控制台",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   },{
     labeltip:'应用资源',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"系统介绍",
       vision:"v2.0",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"校审中心介绍",
       vision:"v1.0",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"产品说明书",
       vision:"v1.0",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_vadio.png",
       name:"教学视频",
       vision:"视频链接",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   }], 
 },{
@@ -51,37 +67,53 @@ const carddata=[{
       img:"./static/img/app_01.png",
       name:"智能打印",
       vision:"易打印",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_1.png",
       name:"人员组织",
       vision:"企业控制台",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_6.png",
       name:"图框管理",
       vision:"企业控制台",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_17.png",
       name:"权限管理",
       vision:"企业控制台",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   },{
     labeltip:'应用资源',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"系统介绍",
       vision:"v1.0",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"产品说明书",
       vision:"v1.0",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"操作手册",
       vision:"v1.0",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_vadio.png",
       name:"教学视频",
       vision:"视频链接",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   }], 
 },{
@@ -91,40 +123,56 @@ const carddata=[{
   blocklist:[{
     labeltip:'全部模块',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_05.png",
       name:"一键签章",
       vision:"易签章",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_03.png",
       name:"智能归档",
       vision:"易归档",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_17.png",
       name:"权限管理",
       vision:"企业控制台",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_8.png",
       name:"签章管理",
       vision:"企业控制台",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   },{
     labeltip:'应用资源',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"系统介绍",
       vision:"帮助中心",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"产品说明书",
       vision:"帮助中心",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"操作手册",
       vision:"下载",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_vadio.png",
       name:"教学视频",
       vision:"视频链接",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   }], 
 },{
@@ -134,40 +182,56 @@ const carddata=[{
   blocklist:[{
     labeltip:'全部模块',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_11.png",
       name:"项目中心",
       vision:"v1.0",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_04.png",
       name:"CAD协同插件",
       vision:"易协同",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/应用分类2.png",
       name:"系统管理",
       vision:"企业控制台",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_data/app_data_icon_10.png",
       name:"数据看板",
       vision:"v1.0",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   },{
     labeltip:'应用资源',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"系统介绍",
       vision:"帮助中心",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"产品说明书",
       vision:"帮助中心",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_way.png",
       name:"协同管理流程",
       vision:"定制",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_vadio.png",
       name:"教学视频",
       vision:"视频链接",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   }], 
 },{
@@ -177,40 +241,56 @@ const carddata=[{
   blocklist:[{
     labeltip:'全部模块',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_21.png",
       name:"Revit协同",
       vision:"轻量化",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_11.png",
       name:"项目中心",
       vision:"BIM协同",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_22.png",
       name:"Revit协同",
       vision:"BIM协同",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_manger/app_manger_icon_10.png",
       name:"族库管理",
       vision:"BIM协同",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   },{
     labeltip:'应用资源',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"TSDP校审文档1",
       vision:"正式版",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"文档2",
       vision:"正式版",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"文档3",
       vision:"正式版",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"文档4",
       vision:"待上线",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   }], 
 },{
@@ -220,40 +300,56 @@ const carddata=[{
   blocklist:[{
     labeltip:'全部模块',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_30.png",
       name:"国产3D平台",
       vision:"TS3D",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_16.png",
       name:"数据中心",
       vision:"平台接口",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_10.png",
       name:"协同管理",
       vision:"多平台协同",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app_17.png",
       name:"交付平台",
       vision:"数字化交付",
+      type:1, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   },{
     labeltip:'应用资源',
     allitems:[{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"系统介绍",
       vision:"正式版",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"产品说明书",
       vision:"正式版",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_pdf.png",
       name:"操作手册",
       vision:"正式版",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     },{
-      img:"./static/img/app_01.png",
+      img:"./static/img/app/app_file_demo.png",
       name:"Demo演示",
       vision:"平台演示",
+      type:2, //1 绿色   2蓝色   3红色   4黄色 5灰色
+      url:'',
     }]
   }], 
 }]
